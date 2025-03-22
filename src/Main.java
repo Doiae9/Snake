88ecd9cb1576a99269fdcda30615db05447f2367
Doiae9 <leonardogh75@gmail.com>
2025-03-22 12:54:50 -0600

    First commit for snake game

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import javax.swing.*;
+
+
+public class Main {
+    public static void main(String[] args) {
+        int boardWidth, boardHeight;
+        boardWidth = 600;
+        boardHeight = boardWidth;
+
+        JFrame frame = new JFrame("snake");
+        frame.setVisible(true);
+    }
+}
